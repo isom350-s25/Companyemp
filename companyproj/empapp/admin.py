0a450b3be5ee5af664d5360b05abde53f37b2def
s2212171344 <abokubr@gmail.com>
2025-04-10 13:12:30 +0300

    Done
    Done

--- companyproj/empapp/admin.py
+++ companyproj/empapp/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Employee
-
-# Register your models here.
-admin.site.register(Employee)
+from .models import Employee  
+ 
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'title', 'salary')  
+ 
+admin.site.register(Employee, EmployeeAdmin)
