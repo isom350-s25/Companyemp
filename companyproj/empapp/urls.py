4c20ef1b3d4eb165a7ba7be440b4b9fe8132c654
Seyed0318 <s22221110318@ku.edu.kw>
2025-04-10 11:55:00 +0300

    Bonus

--- companyproj/empapp/urls.py
+++ companyproj/empapp/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('salary/', views.employee_sal, name='employee_sal'),
     path('pater/', views.employee_by_name, name='employee_by_name'),
     path('born/', views.employee_born_after_1990, name='employee_born_after_1990'),
+    path('duration/', views.employee_hire_duration, name='employee_hire_duration'),
+    path('average/', views.employee_average_salary, name='employee_average_salary'),
 ]
--- companyproj/empapp/views.py
+++ companyproj/empapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.db.models import Avg
 from .models import Employee
+from datetime import date
 
 def employee_list(request):
     employees = Employee.objects.all()  # Fetch all employee records
@@ -16,6 +18,19 @@ def employee_by_name(request):
 def employee_born_after_1990(request):
     employees = Employee.objects.filter(DOB__year__gt=1990)  # Filter employees born after 1990
     return render(request, 'employee_list.html', {'employees': employees})
+
+def employee_hire_duration(request):
+    employees = Employee.objects.all()
+    for employee in employees:
+        if employee.hire_date:  # Check if hire_date is not None
+            employee.hire_duration = (date.today() - employee.hire_date).days
+        else:
+            employee.hire_duration = "N/A"  # Assign "N/A" if hire_date is None
+    return render(request, 'employee_list.html', {'employees': employees, 'show_hire_duration': True})
+
+def employee_average_salary(request):
+    average_salary = Employee.objects.aggregate(Avg('salary'))['salary__avg']  # Calculate average salary
+    return render(request, 'employee_average_salary.html', {'average_salary': average_salary})
 
 
 
