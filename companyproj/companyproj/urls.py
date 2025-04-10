a897f422fadc450c241eb8cff2be187331565fe8
2201125907 <s2201125907@cba.ku.edu.kw>
2025-04-10 11:45:55 +0300

    temp

--- companyproj/companyproj/urls.py
+++ companyproj/companyproj/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from .views import employee_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
--- companyproj/empapp/views.py
+++ companyproj/empapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import Employee
 # Create your views here.
+
+def employee_list(request):
+    employees = Employee.objects.all()  # Fetch all employees
+    return render(request, 'employee_list.html', {'employees': employees})
